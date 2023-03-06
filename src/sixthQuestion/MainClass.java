3c1e01c8102c69850a83668a68e3397ad59a1729
akshay120594 <110121426+akshay120594@users.noreply.github.com>
2023-03-06 17:13:58 +0530

    first commit

---
+++ src/sixthQuestion/MainClass.java
@@ -0,0 +1,13 @@
+package sixthQuestion;
+
+public class MainClass {
+    /* in my last orginasation we were using jsp for rendering frontend 
+    after learning about thymeleaf we replaced jsp with it for rendering
+    html 
+    We implemented second level cashe in hibernate for improving data 
+    retrivel speed in application
+    We implemenrted pagination in hibernate to reduce memory load in 
+    application
+    We were using md5 hashing earlier but security improvement we
+    started using jwt */
+}
